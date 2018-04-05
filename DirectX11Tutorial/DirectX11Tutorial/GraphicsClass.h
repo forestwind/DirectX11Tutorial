e8d7f438f469f32f60d3737e37d50f6687ddb171
Lee jin-Woo <came1230@naver.com>
2018-04-06 04:16:33 +0900

    정점버퍼, 인덱스 버퍼 설정, 쉐이더 , HLSL

--- DirectX11Tutorial/DirectX11Tutorial/GraphicsClass.h
+++ DirectX11Tutorial/DirectX11Tutorial/GraphicsClass.h
@@ -4,12 +4,15 @@
 // GLOBALS //
 ////////////
 
-const bool FULL_SCREEN = true;
+const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
 
 class D3DClass;
+class CameraClass;
+class ModelClass;
+class ColorShaderClass;
 
 class GraphicsClass
 {
@@ -27,5 +30,8 @@ private:
 
 private:
 	D3DClass* m_Direct3D = nullptr;
+	CameraClass* m_Camera = nullptr;
+	ModelClass* m_Model = nullptr;
+	ColorShaderClass* m_ColorShader = nullptr;
 };
 
